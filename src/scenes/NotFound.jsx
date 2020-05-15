7e6eaa3a60aa741af43f3266fd26709066bb0c85
rocky1638 <rockzhou15@gmail.com>
2020-05-14 21:27:52 -0400

    update 404

--- src/scenes/NotFound.jsx
+++ src/scenes/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Text, Button } from "components";
+import { Text, Button, Image } from "components";
 
 const ResponsiveHomeDiv = styled.div`
   padding: 0 25%;
+  text-align: center;
 
   @media only screen and (max-width: 768px) {
     padding: 0 5%;
@@ -17,8 +18,11 @@ const NotFound = () => (
       <Text style={{ marginBottom: 10 }} block fontWeight={500} ginormous>
         Oops!
       </Text>
-      <Text style={{ marginBottom: 20 }} block book gray4>
-        That page doesn't exist. Here's a link to head back home.
+      <Image
+        style={{ margin: 10, marginLeft: 0, height: 300, width: 300 }}
+        src="https://foodiy-beta.s3.amazonaws.com/404.gif" />
+      <Text style={{ marginTop: 10, marginBottom: 20 }} block book gray4>
+        That page doesn't exist. Click the button to head back home.
       </Text>
       <Link to="/">
         <Button>Home</Button>
